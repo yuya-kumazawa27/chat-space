2b96beb76d342a163fc72b6e325d9f7927e50290
yuya-kumazawa27 <61692101+yuya-kumazawa27@users.noreply.github.com>
2020-03-09 21:01:29 +0900

    USERデータベース　作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+=======
+  devise_for :users
+  root "messages#index"
+end
+>>>>>>> Stashed changes
