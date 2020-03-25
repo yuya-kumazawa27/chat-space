909b4a719bbc2b1a1d4aad49592a493f1c64dcc4
yuya-kumazawa27 <61692101+yuya-kumazawa27@users.noreply.github.com>
2020-03-25 16:33:42 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -114,6 +114,6 @@ var reloadMessages = function() {
 };
 //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
 if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-  // setInterval(reloadMessages, 7000);
+  setInterval(reloadMessages, 7000);
 }
 });
